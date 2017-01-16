714e79f61769143f52c5e61097028954fab6cc55
paolatoso <paolaams@yahoo.es>
2017-01-16 10:26:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -96,13 +96,13 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
+    function (homeHtmlUrl) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+    var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -116,18 +116,18 @@ var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+    var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", "'"+chosenCategoryShortName+"'");
     
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+    insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
-}
-console.log(buildAndShowHomeHTML());
+    }
+
 
 // Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
